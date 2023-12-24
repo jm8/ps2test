1de0d3db77e52ecec57f06867ed18167448e228f
josh <jm8hjkl@gmail.com>
2023-12-24 10:06:50 -0500

    use linker script

--- build.zig
+++ build.zig
@@ -1,9 +1,17 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
+pub const PS2BuildInfo = struct {
+    //SDK Path
+    path_to_sdk: []const u8,
+    src_file: []const u8,
+};
+
 pub fn build(b: *std.Build) void {
+    const build_info = PS2BuildInfo{
+        .path_to_sdk = "/usr/local/ps2dev/ps2sdk",
+        .src_file = "src/main.zig",
+    };
+
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -11,7 +19,7 @@ pub fn build(b: *std.Build) void {
     const cross_target = std.zig.CrossTarget{
         .cpu_arch = .mipsel,
         .os_tag = .freestanding,
-        .cpu_model = .{ .explicit = &std.Target.mips.cpu.mips64r5 },
+        .cpu_model = .{ .explicit = &std.Target.mips.cpu.mips3 },
     };
     const target = b.standardTargetOptions(.{
         .default_target = cross_target,
@@ -23,51 +31,15 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "mnt",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .name = "main",
+        .root_source_file = .{ .path = build_info.src_file },
         .target = target,
         .optimize = optimize,
     });
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    b.installArtifact(exe);
+    exe.setLinkerScriptPath(.{ .cwd_relative = b.pathJoin(&[_][]const u8{ build_info.path_to_sdk, "/ee/startup/linkfile" }) });
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
+    const install_exe = b.addInstallArtifact(exe, .{});
 
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+    b.default_step.dependOn(&install_exe.step);
 }
